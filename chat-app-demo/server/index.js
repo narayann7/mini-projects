5a15a9245a22b46e48df7ca566ccc9dcdcde77be
Narayan <laxminarayanreddy432@gmail.com>
2022-05-03 12:50:17 +0700

    added-chat-demo

---
+++ chat-app-demo/server/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3001;
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+app.use(cors());
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "OPTIONS"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected", socket.id);
+
+  socket.on("send_message", (data) => {
+    console.log("data", data);
+
+    socket.broadcast.emit("receive_message", data);
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
